3b96588f15ae27dcb933c139db49a89f1ae924b1
noheul1030 <papooa5140@gmail.com>
2023-04-18 18:00:51 +0900

    Create: OrderList.java

---
+++ 놀이공원 티켓발권 프로그램/src/LOTTEWORLD/OrderList.java
@@ -0,0 +1,22 @@
+package LOTTEWORLD;
+
+public class OrderList {
+	public String id ,optionString;
+	public int age,option,amount,cost;
+	
+//	public List<OrderList> data = new ArrayList<OrderList>();
+//	
+//	
+//	
+//	OrderList order = new OrderList();
+//	order.id = idNumber;
+//	order.age = age;
+//	order.option = preferOption;
+//	order.amount = amount;
+//	order.cost = cost;
+//	data.add(order);
+	
+	for(OrderList order : ls.data) {
+		
+	}
+}
