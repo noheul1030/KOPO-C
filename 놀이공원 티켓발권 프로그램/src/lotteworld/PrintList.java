7e6c23efcaef8bbb995b9c435ed5b92ec672287c
noheul1030 <papooa5140@gmail.com>
2023-04-20 00:48:59 +0900

    REFACTOR: PrintList.java

--- 놀이공원 티켓발권 프로그램/src/lotteworld/PrintList.java
+++ 놀이공원 티켓발권 프로그램/src/lotteworld/PrintList.java
@@ -4,63 +4,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrintList {
-	private static String ticketType;
-	private static String group;
-	private static int ticketCount;
-	private static int totalPrice;
-	private static String preferentialTreatment;
-	private static List<OrderList> data = new ArrayList<OrderList>();
-	
-	public static OrderList orderList = null;
-	
-	static void saveData(TicketDTO dto) {
-		orderList = new OrderList(ticketType, group, ticketCount, totalPrice, preferentialTreatment);
-		
-		if(dto.getTicketChoice() == 1) {
-			orderList.setTicketType("종일권");
-		}else {
-			orderList.setTicketType("야간권");
-		}
-		
-		orderList.setGroup(TicketDTO.PERSON[dto.getPersonGroupNumber()-1]);
-		
-		orderList.setTicketCount(dto.getBuyTicketChoice());
-		
-		orderList.setTotalPrice(dto.getTotalPrice());
-		
-		orderList.setPreferentialTreatment("우대할인" 
-		+ TicketDTO.PREFERENTIAL_TREATMENT_PERSON[dto.getPreferentialTreatment()-1]);	
 
-		data.add(orderList);
-	}
-	
-	public static void ticketTotalPrint(TicketDTO dto) {
-		System.out.println();
-		System.out.println("=========================== LOTTE WORLD ===========================");
-		System.out.printf("%7s%9s%9s%9s%9s\n","권종","그룹","수량","가격","우대사항");
-		System.out.println("===================================================================");
-		System.out.println();
-		OrderListPrint();
-		System.out.println();
-		System.out.println("===================================================================");
-		
-		System.out.printf("입장료 총액은 %s원 입니다."
-				,TicketDTO.STRING_DECIMAL_FORMAT.format(dto.getTotalSumPrice()));
-	}
-	
-	public static void OrderListPrint() {
-		
-		for(OrderList order : data) {
-			ticketType = order.getTicketType();
-			group = order.getGroup();
-			ticketCount = order.getTicketCount();
-			totalPrice = order.getTotalPrice();
-			preferentialTreatment = order.getPreferentialTreatment();
-		}		
-			System.out.printf("%7s",ticketType);
-			System.out.printf("%8s",group);
-			System.out.printf("%9s","X  "+ticketCount);
-			System.out.printf("%12s원",TicketDTO.STRING_DECIMAL_FORMAT.format(totalPrice));
-			System.out.printf("%12s\n","* "+ preferentialTreatment);
-	}
+    private List<OrderList> data = new ArrayList<OrderList>();
+    private OrderList orderList = null;
+
+    // 1. OrderList 클래스 생성자에 dto 값 전달 메서드
+    public void saveData(TicketDTO dto) {
+        String ticketType, group, preferentialTreatment;
+        int ticketCount, totalPrice;
+
+        if (dto.getTicketChoice() == 1) {
+            ticketType = "종일권";
+        } else {
+            ticketType = "야간권";
+        }
+
+        group = TicketDTO.PERSON[dto.getPersonGroupNumber() - 1];
+
+        ticketCount = dto.getBuyTicketChoice();
+
+        totalPrice = dto.getTotalPrice();
+
+        preferentialTreatment = "우대할인 " + TicketDTO.PREFERENTIAL_TREATMENT_PERSON[dto.getPreferentialTreatment() - 1];
+
+        orderList = new OrderList(ticketType, group, ticketCount, totalPrice, preferentialTreatment);
+        data.add(orderList);
+    }
+
+    // 2. 최종 영수증 출력 메서드
+    public void ticketTotalPrint(TicketDTO dto) {
+        System.out.println();
+        System.out.println("======================== LOTTE WORLD ========================");
+        System.out.printf("%5s%9s%9s%9s%14s\n", "권종", "그룹", "수량", "가격", "우대사항");
+        System.out.println("=============================================================");
+
+        OrderListPrint();
+
+        System.out.println("=============================================================");
+        System.out.println();
+        System.out.printf("***** 입장료 총액은 %s원 입니다. *****",
+                TicketDTO.STRING_DECIMAL_FORMAT.format(dto.getTotalSumPrice()));
+    }
+
+    // 3. 누적된 발권정보 출력 메서드
+    public void OrderListPrint() {
+
+        for (OrderList order : data) {
+
+            System.out.printf("%5s", order.getTicketType());
+            System.out.printf("%9s", order.getGroup());
+            System.out.printf("%10s", "X  " + order.getTicketCount());
+            System.out.printf("%12s원", TicketDTO.STRING_DECIMAL_FORMAT.format(order.getTotalPrice()));
+            System.out.printf("%4s%8s\n", " ", "* " + order.getPreferentialTreatment());
+        }
+    }
 }
