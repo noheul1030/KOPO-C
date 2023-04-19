555be6716e5a636489f87bd0f426ec42a935223e
noheul1030 <papooa5140@gmail.com>
2023-04-20 00:50:52 +0900

    STYLE: TicketPriceEnum.java

--- 놀이공원 티켓발권 프로그램/src/lotteworld/TicketPriceEnum.java
+++ 놀이공원 티켓발권 프로그램/src/lotteworld/TicketPriceEnum.java
@@ -7,15 +7,18 @@ public enum TicketPriceEnum {
     private final int dayPrice;
     private final int nightPrice;
 
+    // 1. 생성자
     private TicketPriceEnum(int dayPrice, int nightPrice) {
         this.dayPrice = dayPrice;
         this.nightPrice = nightPrice;
     }
 
+    // 2. 주간권 금액 get
     public int getDayPrice() {
         return this.dayPrice;
     }
 
+    // 3. 야간권 금액 get
     public int getNightPrice() {
         return this.nightPrice;
     }
