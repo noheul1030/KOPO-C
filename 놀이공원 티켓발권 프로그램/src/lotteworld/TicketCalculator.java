d29ba1645351c10c60c7cc22c3f9238c32504f40
noheul1030 <papooa5140@gmail.com>
2023-04-19 18:51:18 +0900

    REFACTOR: TicketCalculator.java

--- 놀이공원 티켓발권 프로그램/src/lotteworld/TicketCalculator.java
+++ 놀이공원 티켓발권 프로그램/src/lotteworld/TicketCalculator.java
@@ -14,50 +14,55 @@ public class TicketCalculator {
         int birthMonth = Integer.parseInt(dto.getResidentRegistrationNumber().substring(2, 4));
         int birthDay = Integer.parseInt(dto.getResidentRegistrationNumber().substring(4, 6));
 
-        int index = Integer.parseInt(dto.getResidentRegistrationNumber().substring(7, 8));
-
-        LocalDate currentDate = LocalDate.now();
+        int index = Integer.parseInt(dto.getResidentRegistrationNumber().substring(7, 8));        
 
         if (index == 3 || index == 4) {
-            birthYear = birthYear + 2000;
+            birthYear = birthYear + TicketDTO.NEW_GENERATION;
         } else {
-            birthYear = birthYear + 1900;
+            birthYear = birthYear + TicketDTO.OLD_GENERATION;
         }
 
         LocalDate birthDate = LocalDate.of(birthYear, birthMonth, birthDay);
 
-        Period agePeriod = Period.between(birthDate, currentDate);
+        Period agePeriod = Period.between(birthDate, TicketDTO.CURRENT_DATE);
 
         age = agePeriod.getYears();
 
-        if (birthMonth > currentDate.getMonthValue()
-                || (birthMonth == currentDate.getMonthValue() && birthDay > currentDate.getDayOfMonth())) {
+        if (birthMonth > TicketDTO.CURRENT_DATE.getMonthValue()
+                || (birthMonth == TicketDTO.CURRENT_DATE.getMonthValue() && birthDay > TicketDTO.CURRENT_DATE.getDayOfMonth())) {
             age--;
         }
-        int result = ticketPrice(dto.getTicketChoice(), ageGroup(age));
-        return total(result, dto.getBuyTicketChoice());
+       
+        int result = ticketPrice(dto.getTicketChoice(), ageGroup(age,dto));
+        int result2 = total(result, dto.getBuyTicketChoice());
+        return (int)(result2 * preferentialTreatmentSale(dto.getPreferentialTreatment()));
     }
 
     // 2. 우대사항 선택에 따른 할인율 초기값 지정 메서드
     public double preferentialTreatmentSale(int preferentialTreatmentChoice) {
-        double[] preferentialTreatmentSale = { 0.0, 1.0, 0.5, 0.5, 0.7, 0.5 };
-
-        return preferentialTreatmentSale[preferentialTreatmentChoice];
+    	
+        return TicketDTO.PREFERENTIAL_TREATMENT_DISCOUNT[preferentialTreatmentChoice-1];
     }
 
     // 3. 나이에 따른 그룹 번호 지정
-    public TicketPriceEnum ageGroup(int age) {
+    public TicketPriceEnum ageGroup(int age,TicketDTO dto) {
 
-        if (18 <= age && age < 65) {
+        if (TicketDTO.MIN_ADULT <= age && age <= TicketDTO.MAX_ADULT) {
+        	dto.setPersonGroupNumber(TicketDTO.ADULT);
             return TicketPriceEnum.ADULT;
-        } else if (13 <= age && age <= 18) {
+        } else if (TicketDTO.MIN_TEEN <= age && age <= TicketDTO.MAX_TEEN) {
+        	dto.setPersonGroupNumber(TicketDTO.TEENAGE);
             return TicketPriceEnum.TEENAGE;
-        } else if (3 <= age && age <= 12) {
+        } else if (TicketDTO.MIN_CHILD <= age && age <= TicketDTO.MAX_CHILD) {
+        	dto.setPersonGroupNumber(TicketDTO.CHILD);
             return TicketPriceEnum.CHILD;
-        } else if (age < 3) {
+        } else if (TicketDTO.MIN_BABY <= age && age < TicketDTO.MIN_CHILD) {
+        	dto.setPersonGroupNumber(TicketDTO.BABY);
             return TicketPriceEnum.BABY;
+        }else {
+        	dto.setPersonGroupNumber(TicketDTO.OLDMAN);
+        	return TicketPriceEnum.OLDMAN;
         }
-        return TicketPriceEnum.OLDMAN;
     }
 
     // 4. 선택한 티켓과 나이를 계산하여 금액을 산출하는 메서드
