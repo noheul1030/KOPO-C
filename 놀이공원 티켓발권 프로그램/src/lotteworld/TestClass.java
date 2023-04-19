e992132800f2624832fc2d149f682874e4027f35
noheul1030 <papooa5140@gmail.com>
2023-04-19 18:50:03 +0900

    REFACTOR: TestClass.java

--- 놀이공원 티켓발권 프로그램/src/lotteworld/TestClass.java
+++ 놀이공원 티켓발권 프로그램/src/lotteworld/TestClass.java
@@ -1,18 +1,15 @@
 package lotteworld;
 
-import java.text.DecimalFormat;
-
 public class TestClass {
-    private DecimalFormat sd = new DecimalFormat("###,###,###,###");
+		
     private InputClass input = new InputClass();
     private TicketDTO dto = new TicketDTO();
-
+    
     public void excute() {
         dto.setTicketChoice(input.ticketChoice());
         dto.setResidentRegistrationNumber(input.residentRegistrationNumber());
         dto.setBuyTicketChoice(input.buyTicketChoice());
         dto.setPreferentialTreatment(input.preferentialTreatmentChoice());
-
     }
 
     public void excute2() {
@@ -27,10 +24,12 @@ public class TestClass {
         excute();
 
         int total = cal.age(dto);
-
-        int total_Sale = (int) (total * cal.preferentialTreatmentSale(dto.getPreferentialTreatment()));
-
-        System.out.println("\n가격은 " + sd.format(total_Sale) + " 원 입니다.");
+        dto.setTotalPrice(total);
+        dto.setTotalSumPrice(total);
+        
+        PrintList.saveData(dto);
+        
+        System.out.println("\n가격은 " + TicketDTO.STRING_DECIMAL_FORMAT.format(total) + " 원 입니다.");
         System.out.println("감사합니다.");
     }
 
@@ -54,5 +53,6 @@ public class TestClass {
                 break;
             }
         }
+        PrintList.ticketTotalPrint(dto);
     }
 }
