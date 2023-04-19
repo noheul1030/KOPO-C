16d039dc955559a347effc845eeeebb239d446de
noheul1030 <papooa5140@gmail.com>
2023-04-20 00:47:53 +0900

    REFACTOR: Class name change

--- 놀이공원 티켓발권 프로그램/src/lotteworld/RunClass.java
+++ 놀이공원 티켓발권 프로그램/src/lotteworld/RunClass.java
@@ -1,48 +1,48 @@
 package lotteworld;
 
-public class TestClass {
-		
+public class RunClass {
+
     private InputClass input = new InputClass();
     private TicketDTO dto = new TicketDTO();
-    
-    public void excute() {
+    private PrintList printlist = new PrintList();
+
+    // 1.입력 값 set 전달 메서드
+    public void insertRunFirst() {
         dto.setTicketChoice(input.ticketChoice());
         dto.setResidentRegistrationNumber(input.residentRegistrationNumber());
         dto.setBuyTicketChoice(input.buyTicketChoice());
         dto.setPreferentialTreatment(input.preferentialTreatmentChoice());
     }
 
-    public void excute2() {
+    // 2. 입력 값 set 전달 메서드2
+    public void insertRunSecond() {
         dto.setReTry(input.reTry());
     }
 
-    // 1. Test 계산 실행 메서드
-    public void totalTest() {
+    // 3. Run 실행 시작 메서드
+    public void startRunFirst() {
         TicketCalculator cal = new TicketCalculator();
 
         // 티켓정보 입력받기
-        excute();
+        insertRunFirst();
 
         int total = cal.age(dto);
         dto.setTotalPrice(total);
         dto.setTotalSumPrice(total);
-        
-        PrintList.saveData(dto);
-        
+
+        printlist.saveData(dto);
+
         System.out.println("\n가격은 " + TicketDTO.STRING_DECIMAL_FORMAT.format(total) + " 원 입니다.");
         System.out.println("감사합니다.");
     }
 
-    // 2. 티켓 발권의 지속 / 종료 여부 확인 메서드
-    public void totalTest2() {
+    // 4. 티켓 발권의 지속 / 종료 여부 확인 메서드
+    public void startRunSecond() {
         PrintClass print = new PrintClass();
         while (true) {
+            startRunFirst();
 
-            // 계산 실행
-            totalTest();
-
-            // 재시도 안내
-            excute2();
+            insertRunSecond();
 
             int pick = dto.getReTry();
 
@@ -53,6 +53,6 @@ public class TestClass {
                 break;
             }
         }
-        PrintList.ticketTotalPrint(dto);
+        printlist.ticketTotalPrint(dto);
     }
 }
