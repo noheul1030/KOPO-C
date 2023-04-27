fa216d46cca8c6600a9e64b7a518e59280046dba
noheul1030 <papooa5140@gmail.com>
2023-04-27 16:55:09 +0900

    REFACTOR: TicketPriceEnum.java

--- 놀이공원 티켓발권 프로그램/src/dto/TicketPriceEnum.java
+++ 놀이공원 티켓발권 프로그램/src/dto/TicketPriceEnum.java
@@ -2,26 +2,33 @@ package dto;
 
 public enum TicketPriceEnum {
 
-    BABY("영아",15000, 15000),
-	CHILD("어린이",46000, 35000),
-	TEENAGE("청소년",52000, 41000),
-	ADULT("성 인",59000, 47000),
-	OLDMAN("노 인",46000, 35000);
+    BABY("BABY","영아",15000, 15000),
+	CHILD("CHILD","어린이",46000, 35000),
+	TEENAGE("TEENAGE","청소년",52000, 41000),
+	ADULT("ADULT","성 인",59000, 47000),
+	OLDMAN("OLDMAN","노 인",46000, 35000);
 
-	private final String personGroup;
+	private final String personGroup_US;
+	private final String personGroup_KR;
     private final int dayPrice;
     private final int nightPrice;
 
     // 1. 생성자
-    private TicketPriceEnum(String personGroup,int dayPrice, int nightPrice) {
-    	this.personGroup = personGroup;
+    private TicketPriceEnum(String personGroup_US, String personGroup_KR, int dayPrice, int nightPrice) {
+    	this.personGroup_US = personGroup_US;
+    	this.personGroup_KR = personGroup_KR;
         this.dayPrice = dayPrice;
         this.nightPrice = nightPrice;
     }
     
-    // 2. 연령별 그룹명 get
-    public String getPersonGroup() {
-    	return this.personGroup;
+    // 2-1. 연령별 그룹명 get
+    public String getPersonGroup_KR() {
+    	return this.personGroup_KR;
+    }
+    
+    // 2-2. 연령별 그룹명 get
+    public String getPersonGroup_US() {
+    	return this.personGroup_US;
     }
 
     // 3. 주간권 금액 get
