02bbabef21d67f0fc565b14da94d9d84c86589a8
noheul1030 <papooa5140@gmail.com>
2023-04-27 16:54:41 +0900

    REFACTOR: InputClass.java

--- 놀이공원 티켓발권 프로그램/src/inputUtil/InputClass.java
+++ 놀이공원 티켓발권 프로그램/src/inputUtil/InputClass.java
@@ -2,74 +2,87 @@ package inputUtil;
 
 import java.util.Scanner;
 
-import output.PrintClass;
+import output.LanguagePrintClass;
 
 public class InputClass {
 
 	private static Scanner scanner = new Scanner(System.in);
 
+	// 0. 언어 선택 메서드
+	public String languageChoice() {
+		while (true) {
+			System.out.println("언어를 선택해 주세요. \n" + "1. KOREAN \n" + "2. ENGLISH ");
+			String languageChoice = scanner.next();
+
+			if (InputErrorCheck.languageChoiceCheck(languageChoice)) {
+				return LanguagePrintClass.choiceLanguage(languageChoice);
+			}
+			System.out.println(LanguagePrintClass.getMsg("Print.error"));
+		}
+	}
+
 	// 1. 티켓 종류 입력 받는 메서드
 	public int ticketChoice() {
 		while (true) {
-			PrintClass.ticketChoicePrint();
+			System.out.println(LanguagePrintClass.getMsg("Print.type"));
 			int ticketChoice = scanner.nextInt();
 
 			if (InputErrorCheck.ticketChoiceCheck(ticketChoice)) {
 				return ticketChoice;
 			}
-			PrintClass.errorPrint();
+			System.out.println(LanguagePrintClass.getMsg("Print.error"));
 		}
 	}
 
 	// 2. 주민등록번호를 입력 받는 메서드
 	public String residentRegistrationNumber() {
 		while (true) {
-			PrintClass.residentRegistrationNumberPrint();
+			System.out.println(LanguagePrintClass.getMsg("Print.residentRegistrationNumber"));
 			String residentRegistrationNumber = scanner.next();
 
 			if (InputErrorCheck.residentRegistrationNumberCheck(residentRegistrationNumber)) {
 				return residentRegistrationNumber;
 			}
-			PrintClass.errorPrint();
+			System.out.println(LanguagePrintClass.getMsg("Print.error"));
 		}
 	}
 
 	// 3. 주문 갯수를 입력 받는 메서드
 	public int buyTicketChoice() {
 		while (true) {
-			PrintClass.buyTicketChoicePrint();
+			System.out.println(LanguagePrintClass.getMsg("Print.buyTicket"));
 			int buyTicketChoice = scanner.nextInt();
 
 			if (InputErrorCheck.buyTicketChoiceCheck(buyTicketChoice)) {
 				return buyTicketChoice;
 			}
-			PrintClass.errorPrint();
+			System.out.println(LanguagePrintClass.getMsg("Print.error"));
 		}
 	}
 
 	// 4. 우대사항을 입력 받는 메서드
 	public int preferentialTreatmentChoice() {
 		while (true) {
-			PrintClass.preferentialTreatmentChoicePrint();
+			System.out.println(LanguagePrintClass.getMsg("Print.preferentialTreatment"));
 			int preferentialTreatmentChoice = scanner.nextInt();
 
 			if (InputErrorCheck.preferentialTreatmentChoiceCheck(preferentialTreatmentChoice)) {
 				return preferentialTreatmentChoice;
 			}
-			PrintClass.errorPrint();
+			System.out.println(LanguagePrintClass.getMsg("Print.error"));
 		}
 	}
 
 	// 5. 재시작 여부를 입력받는 메서드
 	public int reTry() {
 		while (true) {
-			PrintClass.totalRePrint();
+			System.out.println(LanguagePrintClass.getMsg("Print.retry"));
 			int reTry = scanner.nextInt();
 
 			if (InputErrorCheck.reTryCheck(reTry)) {
 				return reTry;
 			}
-			PrintClass.errorPrint();
+			System.out.println(LanguagePrintClass.getMsg("Print.error"));
 		}
 	}
 }
